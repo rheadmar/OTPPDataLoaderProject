f432a873a2c18b0041c365722dec933b3e2453fd
rheadmar <rheadmar@uwaterloo.ca>
2020-06-14 21:40:49 -0400

    Adding mutual exclusion to the argparser to disable users from entering multilple different commands to the client as parameters.

--- client.py
+++ client.py
@@ -26,8 +26,11 @@ def main(arguments):
 		req = json.dumps({'action' : 'reset'})
 	else:
 		return 1
-		
-	asyncio.get_event_loop().run_until_complete(send_request(uri,req))
+	try:
+		asyncio.get_event_loop().run_until_complete(send_request(uri,req))
+	except:
+		#TODO add send Email code here
+		exit(1)
 
 
 async def send_request(uri,req):
@@ -42,11 +45,12 @@ async def send_request(uri,req):
 
 if __name__ == "__main__":
 	argparser = argparse.ArgumentParser(description="""This is a script which connects and can be used to query or modify a host process that is building signals for wether to buy or sell stocks.""")
-	argparser.add_argument("--price",dest="price",metavar = "YYYY-MM-DD-HH:MM",default = None,help='These are the tickers for which data will be gathered and accumulated')
-	argparser.add_argument("--signal",dest="signals",metavar = "YYYY-MM-DD-HH:MM", default = None,help='This call will return the most recent data for all stocks that were subscribed at the given time')
+	group = argparser.add_mutually_exclusive_group()
+	group.add_argument("--price",dest="price",metavar = "YYYY-MM-DD-HH:MM",default = None,help='These are the tickers for which data will be gathered and accumulated')
+	group.add_argument("--signal",dest="signals",metavar = "YYYY-MM-DD-HH:MM", default = None,help='This call will return the most recent data for all stocks that were subscribed at the given time')
 	argparser.add_argument("--server_address",dest="serverAdd",metavar='192.168.0.1:8052',default = "localhost:8000",help='This is the IP address and port at which you can reach the server you are attempting to access data from.')
-	argparser.add_argument("--del_ticker",dest="delticker",metavar='AAPL',help='This call will remove monitoring for a given ticker.Return codes 0=success, 1=server error, 2=ticker not found')
-	argparser.add_argument("--add_ticker",dest="newticker",metavar='AAPL',help='This will inform the server to start monitoring this ticker and report signals for this ticker. Return code 0=success, 1=error, 2=invalid ticker')
-	argparser.add_argument("--reset",dest="reset",action='store_const',const = True, default = None,help='Informs the server to reload all data from source vendors and recalculates signals')
+	group.add_argument("--del_ticker",dest="delticker",metavar='AAPL',help='This call will remove monitoring for a given ticker.Return codes 0=success, 1=server error, 2=ticker not found')
+	group.add_argument("--add_ticker",dest="newticker",metavar='AAPL',help='This will inform the server to start monitoring this ticker and report signals for this ticker. Return code 0=success, 1=error, 2=invalid ticker')
+	group.add_argument("--reset",dest="reset",action='store_const',const = True, default = None,help='Informs the server to reload all data from source vendors and recalculates signals')
 	args = argparser.parse_args()
 	main(args)
